47e154cde258c822abd8a5c4998ba805fbb8b4b5
Aashijit <aashijitM@gmail.com>
2019-08-27 18:20:40 +0530

    Added Sign Up and Splash Screen

---
+++ app/src/main/java/team/exp/dimagsekhelo/Activity/SignUpScreen.java
@@ -0,0 +1,20 @@
+package team.exp.dimagsekhelo.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import team.exp.dimagsekhelo.R;
+
+public class SignUpScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_screen);
+    }
+
+    public void signUpBackButton(View view) {
+        this.finish();
+    }
+}
---
+++ app/src/main/java/team/exp/dimagsekhelo/Activity/SplashScreen.java
@@ -0,0 +1,35 @@
+package team.exp.dimagsekhelo.Activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import team.exp.dimagsekhelo.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+
+
+
+
+
+    //UI Connector methods start from here
+    public void splashScreenLetsPlay(View view) {
+
+    }
+
+    public void splashScreenLogIn(View view) {
+
+    }
+
+    public void splashScreenSignUp(View view) {
+        Intent intent = new Intent(SplashScreen.this, SignUpScreen.class);
+        startActivity(intent);
+    }
+}
