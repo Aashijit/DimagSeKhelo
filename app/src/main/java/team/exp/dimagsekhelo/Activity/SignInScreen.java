7367eeaa897e4557835a6c3c5c1ad85a3f0f6aa0
Aashijit Mukhopadhyay <aashijitM@gmail.com>
2019-09-02 23:21:18 +0530

    Minor changes in function name

--- app/src/main/java/team/exp/dimagsekhelo/Activity/SignInScreen.java
+++ app/src/main/java/team/exp/dimagsekhelo/Activity/SignInScreen.java
@@ -14,16 +14,13 @@ public class SignInScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_screen);
     }
-    public void signInfotgotPassword(View view) {
-
-        Intent intent=new Intent(SignInScreen.this, ForgotPassword.class);
-        startActivity(intent);
-
-    }
     public void signInBackButton(View view) {
         this.finish();
     }
 
 
-
+    public void signInForgotPassword(View view) {
+        Intent intent=new Intent(SignInScreen.this, ForgotPassword.class);
+        startActivity(intent);
+    }
 }
