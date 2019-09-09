70796cf1787a84c7b3f0ea6435ced8fc94d5f428
Aashijit Mukhopadhyay <aashijitM@gmail.com>
2019-09-09 18:04:26 +0530

    Added design for the My Profile Page

---
+++ app/src/main/java/team/exp/dimagsekhelo/Activity/MyProfileActivity.java
@@ -0,0 +1,15 @@
+package team.exp.dimagsekhelo.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import team.exp.dimagsekhelo.R;
+
+public class MyProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+    }
+}
