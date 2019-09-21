c37e16dc701b1bd7085e47b4b3d4451d4b55a04c
Aashijit Mukhopadhyay <aashijitM@gmail.com>
2019-09-21 20:22:16 +0530

    minor changes

---
+++ app/src/main/java/team/exp/dimagsekhelo/Activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package team.exp.dimagsekhelo.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import team.exp.dimagsekhelo.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
