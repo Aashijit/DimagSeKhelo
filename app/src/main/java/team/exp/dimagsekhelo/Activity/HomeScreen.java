34a56e7fcb847e75538927d9b0616463f63fd353
Aashijit Mukhopadhyay <aashijitM@gmail.com>
2019-09-28 08:36:56 +0530

    Added Home Screen

--- app/src/main/java/team/exp/dimagsekhelo/Activity/HomeScreen.java
+++ app/src/main/java/team/exp/dimagsekhelo/Activity/HomeScreen.java
@@ -1,65 +1,55 @@
 package team.exp.dimagsekhelo.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
-
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import team.exp.dimagsekhelo.R;
 
 public class HomeScreen extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
+
+
+
+    //Member Variables
+    private FirebaseAuth firebaseAuth;
+    //Member Variables
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
+
+        //Initialize the variables
+        firebaseAuth = FirebaseAuth.getInstance();
+        //Initialize the variables
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(HomeScreen.this, MyProfileScreen.class);
+                startActivity(intent);
             }
         });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home_screen, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+    public void logOut(View view) {
+        firebaseAuth.signOut();
+
+        Intent intent = new Intent(HomeScreen.this, SplashScreen.class);
+        finish();
+        startActivity(intent);
     }
 }
